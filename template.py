1fee923ec0c6f295ef816594f9a0cd66e1da2b63
alextjanse <github@alexjanse.nl>
2025-12-09 11:56:49 +0100

    template added

---
+++ template.py
@@ -0,0 +1,16 @@
+from typing import List
+from aoc import read_file
+
+def get_data() -> List[str]:
+    return read_file(1, True)
+
+def solve_part_1(data):
+    return 0
+
+def solve_part_2(data):
+    return 0
+
+if __name__ == "__main__":
+    data = get_data()
+    solve_part_1(data)
+    solve_part_2(data)
